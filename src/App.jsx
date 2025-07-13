2ed03cb9a39d98f89becffee53ed56bb14e42911
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-13 19:31:21 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { Outlet, useSearchParams } from 'react-router-dom'
 import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Header , Footer} from './component'
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog with appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){dispatch(login({userData}))
+      }else{
+    dispatch(logout())}
+    })
+    .finally(()=>{setloading(false)})
+  },[])
+
+  return !loading ? (
+<div className='min-h-screen flex flex-wrap content-between
+bg-gray-400'>
+  <div className='w-full block'>
+    <Header/>
+    <main>
+      Blog{/* <Outlet/> */}
+    </main>
+    <Footer/>
+  </div>
+</div>
+  ): "null"
 }
 
 export default App
